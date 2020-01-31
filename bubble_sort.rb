9a298831f5d1bc8611e96a5f4d28d90377885843
sz16900 <sz16900@my.bristol.ac.uk>
2020-01-31 13:26:21 -0500

    :recycle: changing values
    change values of bubble_sort.rb

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,12 +1,6 @@
-my_array1 = [3, 1, 2, 1, 9, 10, 7, 8, 120, 1]
-
-my_array2 = ["z", "x", "a", "b", "q", "y"]
-
-
 def bubble_sort(arr)
 
     len = arr.length
-
     lil_len = len-1
 
     until lil_len == 0
@@ -17,13 +11,14 @@ def bubble_sort(arr)
         end
         lil_len -= 1
     end
+    
 end
 
 
+# Testing 
+my_array1 = [3, 1, 2, 1, 9, 10, 7, 8, 120, 1]
+my_array2 = ["z", "x", "a", "b", "q", "y"]
 bubble_sort(my_array1)
-
 puts my_array1
-
 bubble_sort(my_array2)
-
 puts my_array2
